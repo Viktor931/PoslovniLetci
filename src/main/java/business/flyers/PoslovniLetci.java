3e1eacb59c2abf8270f4ce0c4ce92261b8d3b7d8
unknown <vmalab@foi.hr>
2018-03-12 19:33:48 +0100

    ***INITIAL COMMIT***

---
+++ src/main/java/business/flyers/PoslovniLetci.java
@@ -0,0 +1,11 @@
+package business.flyers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovniLetci {
+    public static void main(String[] args){
+        SpringApplication.run(PoslovniLetci.class, args);
+    }
+}
